c5e4c5bf30eb2627a54a58eda2b546db33cb7661
me-suzy <me.suzana@gmail.com>
2025-08-10 23:23:57 +0300

    Add pafiledb_3.0-final/pafiledb_3.0-final/lang/arabic.php

---
+++ pafiledb_3.0-final/pafiledb_3.0-final/lang/arabic.php
@@ -0,0 +1,266 @@
+<?php
+/*
+  paFileDB 3.0
+  ©2001 PHP Arena
+  Written by Todd
+  todd@phparena.uni.cc
+  http://www.phparena.uni.cc
+  Keep all copyright links on the script visible
+  Please read the license included with this script for more information.
+
+  ##########
+  #paFileDB 3.0 Language File
+  #Arabic
+  #Translated by Malek El7azeen Email: sad_boy3000@hotmail.com
+  ##########
+
+  ##########	
+  #If you are translating this file, DO NOT translate anything in brackets, such as {something}, any variables ($something) or arrays #($something[somethingelse]), or HTML tags (<something></something>) or else parts of the script will not work
+  #Also, do not delete any lines in this script
+  ##########
+
+*/
+
+//Start Beta 2 Language File
+$str[time] = "ÈÊæÞíÊ";
+$str[power] = "ÈÏÚã ãä";
+$str[exectime] = "ÇÓÊÛÑÞ ÊÍãíá ÇáÕÝÍÉ";
+$str[numqueries] = "MySQL Queries Used";
+$str[adminops] = "ÎíÇÑÇÊ ÇáÇÏÇÑÉ";
+$str[mainpage] = "ÇáÕÝÍÉ ÇáÑÆíÓíÉ";
+$str[addcat] = "ÇÖÝ ÞÓã";
+$str[editcat] = "ÍÑÑ ÞÓã";
+$str[delcat] = "ÇÍÐÝ ÞÓã";
+$str[ordercat] = "ÇÚÏ ÊÑÊíÈ ÇáÇÞÓÇã";
+$str[category] = "ÇáÞÓã";
+$str[files] = "ÇáãáÝÇÊ";
+$str[jump] = "ÇáÇäÊÞÇá ÇáÓÑíÚ";
+$str[logged] = "Êã ÊÓÌíá ÏÎæáß íÇ";
+$str[admincenter] = "áæÍÉ ÇáÊÍßã";
+$str[logout] = "ÊÓÌíá ÇáÎÑæÌ";
+$str[file] = "ÇáãáÝ";
+$str[date] = "ÊÇÑíÎ ÇáÇÖÇÝÉ";
+$str[rating] = "ÇáÊÑÔíÍ";
+$str[dls] = "ÇáÊÍãíá";
+$str[email] = "ÇÎÈÑ ÕÏíÞÉ Úä åÐÇ ÇáãáÝ";
+$str[emailinfo] = "ÇÐÇ ßäÊ ÊÑÛÈ ÇÎÈÇÑ ÇÍÏ ÇÕÏÞÇÆß Úä åÐÇ ÇáãáÝ, ÊÓÊØíÚ ÊÚÈÆÉ ÇáÈíÇäÇÊ ÇáÊÇáíÉ æ ÓæÝ äÞæã ÈÇÑÓÇá ßÇÝÉ ãÚáæãÇÊ ÇáãáÝ Çáì ÕÏíÞß!";
+$str[yname] = "ÇÓãß";
+$str[yemail] = "ÈÑíÏß ÇáÇáíßÊÑæäí";
+$str[fname] = "ÇÓãÇÁ ÇÕÏÞÇÆß";
+$str[femail] = "ÇíãíáÇÊ ÇÕÏÞÇÆß";
+$str[esub] = "ãæÖæÚ ÇáÑÓÇáÉ";
+$str[etext] = "äÕ ÇáÑÓÇáÉ";
+$str[defaultmail] = "ÇÚÊÞÏ Çäß ÓÊÓãÊÚ ÈÊÍãíá æ ÇÓÊÎÏÇã åÐÇ ÇáãáÝ:";
+$str[semail] = "ÇÑÓá ÇáÈÑíÏ";
+$str[msg] .= "ãÑÍÈÇ $friend[name],\n";
+$str[msg] .= "$email[message]\n\n";
+$str[msg] .= "----------\n";
+$str[msg] .= "áÞÏ æÕáÊß åÐå ÇáÑÓÇáÉ ãä  \"{dbname}\" . ãÚ ÇáÚáã ÈÃä \"{dbname}\" ÛíÑ ãÓÄæá Úä ãÍÊæì åÐå ÇáÑÓÇáÉ ÈÊÇÊÇ.";
+$str[econf] = "ÔßÑÇ áß! áÞÏ Êã ÇÑÓÇá ÑÓÇáÊß $friend[name]'s.";
+$str[editfile] = "ÍÑÑ ãáÝ";
+$str[deletefile] = "ÇÍÐÝ ãáÝ";
+$str[desc] = "ÇáæÕÝ";
+$str[creator] = "ãáßíÉ ÇáãáÝ";
+$str[version] = "ÑÞã ÇáÇÕÏÇÑÉ";
+$str[scrsht] = "ÕæÑÉ Úäå";
+$str[docs] = "ãáÝ ÇáÊÚáíãÇÊ";
+$str[lastdl] = "ÇÎÑ ÊÍãíá";
+$str[never] = "ÇÈÏÇ";
+$str[votes] = "ÇáÊÑÔíÍÇÊ";
+$str[download] = "Íãá ÇáãáÝ";
+$str[rate] = "ÑÔÍ ÇáãáÝ";
+$str[license] = "ÇáÇÊÝÇÞíÉ æ ÇáÊÑÇÎíÕ";
+$str[licensewarn] = "íÌÈ Úáíß ÇáãæÇÝÞÉ  Úáì åÐå ÇáÇÊÝÇÞíÉ ÍÊì ÊÓÊØíÚ ÊÍãíá åÐÇ ÇáãáÝ";
+$str[iagree] = "ãæÇÝÞ";
+$str[dontagree] = "ÛíÑ ãæÇÝÞ";
+$str[rerror] = "ÚÝæÇ, áÞÏ ÞãÊ ÈÇáÊÕæíÊ áåÇ ÇáãáÝ ãä ÞÈá.";
+$str[rateinfo] = "ÇäÊ Úáì æÔß ÇáÊÕæíÊ áÜ <i>{filename}</i>. ÇáÑÌÇÁ ÇÎÊÑ ãÓÊæì ÇáãáÝ ãÚ ÇáÚáã Çä ÇáÏÑÌÉ . 1 ÊÚäí Çä ÇáãáÝ ÓíÁ ÌÏÇ, 10 æ ÇáÏÑÌÉ  ÊÚäí Çä ÇáãáÝ ããÊÇÒ.";
+$str[rconf] = "áÞÏ ÞãÊ ÈÇÖÇÝÉ ÊÕæíÊß áÜ <i>{filename}</i>  {rate}. æ åÐÇ íÌÚá ãÌãæÚ ÇáÊÑÔíÍÇÊ áåÐÇ ÇáãáÝ ÊÕá Çáì $nrating {newrating}/10.";
+$str[stats] = "ãÚáæãÇÊ ÚÇãÉ";
+$str[statstext] .= "íæÌÏ  $num[files] ãáÝ / ãáÝÇÊ Ýí  $num[cats] ãÌáÏ / ãÌáÏÇÊ<br>";
+$str[statstext] .= "ÇÞÏã ãáÝ åæ <a href=\"pafiledb.php?action=file&id= $oldest[file_id]\"> $oldest[file_name]</a><br>";
+$str[statstext] .= "ÇÍÏË ãáÝ åæ <a href=\"pafiledb.php?action=file&id= $newest[file_id]\"> $newest[file_name]</a><br>";
+$str[statstext] .= "ÇÓæÃ ãáÝ ãä ÍíË ÊÑÔíÍÇÊ ÇáÒæÇÑ åæ <a href=\"pafiledb.php?action=file&id= $lpopular[file_id]\"> $lpopular[file_name]</a> æ äÓÈÉ ÇáÊÑÔíÍ åí $least/10<br>";
+$str[statstext] .= "ÇÝÖá ãáÝ ãä ÍíË ÊÑÔíÍÇÊ ÇáÒæÇÑ åæ <a href=\"pafiledb.php?action=file&id= $popular[file_id]\"> $popular[file_name]</a> æ äÓÈÉ ÇáÊÑÔíÍ åí  $most/10<br>";
+$str[statstext] .= "ÇÓæÃ ãáÝ ãä ÍíË ÚÏÏ ÇáÊÍãíá åæ <a href=\"pafiledb.php?action=file&id= $leastdl[file_id]\"> $leastdl[file_name]</a> æ ÚÏÏ ÇáÊÍãíáÇÊ  $leastdl[file_dls] ãÑÉ<br>";
+$str[statstext] .= "ÇÝÖá ãáÝ ãä ÍíË ÚÏÏ ÇáÊÍãíá åæ <a href=\"pafiledb.php?action=file&id= $mostdl[file_id]\"> $mostdl[file_name]</a> æ ÚÏÏ ÇáÊÍãíáÇÊ  $mostdl[file_dls] ãÑÉ<br>";
+$str[statstext] .= "ÇáãÌãæÚ Çáßáí áÚÏÏ ÇáÊÍãíáÇÊ åæ  $totaldls[0] ãÑÉ<br>";
+$str[statstext] .= "ãÚÏá ÇáÊÕæíÊ Úáì ÇáãáÝÇÊ åæ  $avg/10<br>";
+$str[statstext] .= "ãÚÏá ÇáÊÍãíá áßá ÈÑäÇãÌ åæ  $avgdls<br>";
+$str[search] = "ÇáÈÍË";
+$str[results] = "äÊíÌÉ ÇáÈÍË Úä";
+$str[nomatches] = "ÚÝæÇ ¡ áã äÌÏ Ãí ãÚáæãÉ ãØÇÈÞÉ áÈÍËß";
+$str[matches] = "äÊÇÆÌ ÇáÈÍË Úä";
+$str[sfor] = "ÇÈÍË Úä";
+$str[viewall] = "ãÔÇåÏÉ ßá ÇáãáÝÇÊ";
+$str[vainfo] = "ãÔÇåÏÉ ßá ÇáãáÝÇÊ Ýí ãßÊÈÉ ÇáÈÑÇãÌ";
+$str[next] = "ÇáÊÇáí";
+$str[prev] = "ÇáÓÇÈÞ";
+$str[pagenums] = "ÕÝÍÇÊ";
+//You have reached Line 100. If you're not tired now, you will be soon.
+$str[acinfo] = "ãÑÍÈÇ Èßã Ýí áæÍÉ ÇáÊÍßã ÇáÎÇÕÉ ÈÈÑäÇãÌ ÇáÈÇÝÇíá 3 ÇáãÚÑÈ. ÊÓÊØíÚ ÇáÇä ÇáÊÍßã Ýí ßÇÝÉ ÇáÈÑäÇãÌ æ ÊÎÕíÕå ÈãÇ íäÇÓÈ ãæÞÚß ßãÇ ÊÓÊØíÚ ÇíÖÇ ÇáÊÍßã Ýí ÇáÇÞÓÇã ãä ÇÖÇÝÉ æ ÍÐÝ æ ÊÚÏíá æ ÇáÊÍßã ÇíÖÇ Ýí ÇáãáÝÇÊ ãä ÇÖÇÝÉ æ ÊÚÏíá æ ÍÐÝ.";
+$str[cmanage] = "ÇÏÇÑÉ ÇáÇÞÓÇã";
+$str[fmanage] = "ÇÏÇÑÉ ÇáãáÝÇÊ";
+$str[cumanage] = "ÇÏÇÑÉ æ ÊÎÕíÕ ÇáÍÞæá";
+$str[lmanage]= "ÇáÇÊÝÇÞíÇÊ æ ÇáÊÑÇÎíÕ";
+$str[amanage] = "ÇÏÇÑÉ ÍÓÇÈÇÊ ÇáãÔÑÝíä";
+$str[csettings] = "ÊÛííÑ ÇáÎÕÇÆÕ";
+$str[asettings] = "ÇÚÏÇÏÇÊ ÍÓÇÈß ÇáÔÎÕí";
+$str[utils] = "ÇÚÏÇÏÇÊ ÚÇãÉ";
+$str[aminfo] = "Êãßäß åÐå ÇáÇÖÇÝÉ ãä ÇÖÇÝÉ æ ÊÍÑíÑ Çæ ÍÐÝ ãÔÑÝíä ÇÎÑíä ááÈÇÝÇíá 3 ÇáãÚÑÈ.";
+$str[aadmin] = "ÇÖÝ ãÔÑÝ";
+$str[eadmin] = "ÍÑÑ ãÔÑÝ";
+$str[dadmin] = "ÇÍÐÝ ãÔÑÝ";
+$str[un] = "ÇÓã ÇáãÓÊÎÏã";
+$str[uninfo] = "åÐÇ ÇáÇÓã ÇáÐí ÓæÝ íÓÊÎÏãå ÇáãÔÑÝ ÇáÌÏíÏ ááÏÎæá Çáì áæÍÉ ÇáÊÍßã";
+$str[aemail] = "ÈÑíÏ ÇáãÔÑÝ ÇáÇáíßÊÑæäí";
+$str[aemailinfo] = "ÊÃßÏ ãä ÕÍÉ ÇáÈÑíÏ ";
+$str[apass] = "ßáãÉ ÇáãÑæÑ";
+$str[apassinfo] = "åäÇ ÊÖÚ ßáãÉ ÇáãÑæÑ ÇáÎÇÕÉ ÈÇáãÔÑÝ ÇáÌÏíÏ æ ÇáÊí ÓæÝ íÓÊÎÏãåÇ ááÏÎæá Çáì áæÍÉ ÇáÊÍßã ßãÇ Çäß ÊÓÊØíÚ ÊÛííÑåÇ ãÊì ÔÆÊ";
+$str[aeditperm] = "ÕáÇÍíÇÊ ÇáãÔÑÝ ÇáÌÏíÏ";
+$str[aeditperminfo] = "ÇÎÊÑ ÇÐÇ ßÇä ÇáãÔÑÝ ÇáÌÏíÏ íÓÊØíÚ ÊÍÑíÑ ÍÓÇÈÇÊ ÇáÔÑÝíä ÇáÇÎÑíä.";
+$str[yes] = "äÚã";
+$str[no] = "áÇ";
+$str[aadderror] = "íæÌÏ ãÔÑÝ ÇÎÑ ááÈÇÝÇíá 3 ÇáãÚÑÈ íÍãá äÝÓ ÇáÇÓã ÇáÑÌÇÁ ÇÎÊíÇÑ ÇÓã ÇÎÑ $form[username]";
+$str[adminadded] = "ÇáãÔÑÝ ÇáÌÏíÏ <i>$form[username]</i> ÊãÊ ÇÖÇÝÊå ÈäÌÇÍ!";
+$str[changepass] = "ÊÛííÑ ßáãÉ ÇáãÑæÑ";
+$str[newpass] = "ßáãÉ ÇáãÑæÑ ÇáÌÏíÏÉ";
+$str[editerror] = "áã ÊÞã ÈÇÎÊíÇÑ ÇáãÔÑÝ ÇáÐí ÊæÏ ÊÍÑíÑ ÈíÇäÇÊå!";
+$str[infochanged] = "Êã ÊÛííÑ ÈíÇäÇÊ ÇáãÔÑÝ ÈäÌÇÍ!";
+$str[passchanged] = "Êã ÊÛííÑ ßáãÉ ÇáãÑæÑ ÇáÎÇÕÉ ÈÇáãÔÑÝ ÈäÌÇÍ!";
+$str[delerror] = "áã ÊÞã ÈÇÎÊíÇÑ ÇáãÔÑÝ ÇáÐí ÊæÏ ÍÐÝå!";
+$str[deleted] = "ÇáãÔÑÝ / ÇáãÔÑÝíä ÇáÐíä ÇÎÊÑÊåã Êã ÍÐÝåã ÈäÌÇÍ.";
+$str[cmaninfo] = "ÊÓÊØíÚ ÇÓÊÎÏÇã åÐÇ ÇáÞÓã ãä áæÍÉ ÇáÊÍßã áÇÖÇÝÉ Çæ ÊÚÏíáÇæ ÍÐÝ ÈÇáÇÖÇÝÉ Çáì ÊÑÊíÈ ÇÞÓÇã ÇáÈÇÝÇíá 3 ÇáãÚÑÈ.";
+$str[acat] = "ÇÖÝ ÞÓã";
+$str[ecat] = "ÍÑÑ ÞÓã";
+$str[dcat] = "ÇÍÐÝ ÞÓã";
+$str[rcat] = "ÑÊÈ ÇáÇÞÓÇã";
+$str[catadded] = "áÞÏ ÊãÊ ÇÖÇÝÉ ÇáÞÓã ÇáÌÏíÏ ÈÇÓã, <i>$form[name]</i>.";
+$str[catname] = "ÇÓã ÇáÞÓã";
+$str[catnameinfo] = "åÐÇ ÇáÇÓã Óíßæä ÇáÇÓã ÇáÐí íÚÑÝ Èå ÇáÞÓã ÇáÌÏíÏ.";
+$str[catdesc] = "æÕÝ ÇáÞÓã";
+$str[catdescinfo] = "áÔÑÍ ãÍÊæíÇÊ ÇáÞÓã";
+$str[catparent] = "ÞÓã ÏÇÎáí";
+$str[catparentinfo] = "ÇÐÇ ÇÑÏÊ ÇÖÇÝÉ åÐÇ ÇáÞÓã Çáì ÞÓã ÇÎÑ, ÇÎÊÑ ÇáÞÓã ÇáÐí ÊæÏ ÇÖÇÝÉ ÇáÞÓã ÇáÌÏíÏ Çáíå.";
+$str[none] = "ÈáÇ";
+$str[catedited] = "áÞÏ Êã ÊÍÑíÑ ÇáÞÓã , <i>$form[name]</i> ÈäÌÇÍ!";
+$str[delfiles] = "åá ÊæÏ ÍÐÝ ÇáãáÝÇÊ ÇáãæÌæÏ Ýí ÇáÞÓã ¿";
+$str[catsdeleted] = "áÞÏ Êã ÍÐÝ ÇáÞÓã ÇáÐí ÇÎÊÑÊå.";
+$str[cdelerror] = "áã ÊÞã ÈÇÎÊíÇÑ ÇáÞÓã ÇáÐí ÊÑÛÈ ÈÍÐÝå!";
+$str[rcatinfo] = "ÊÓÊØíÚ ÊÛííÑ ÊÑÊíÈ ÇáÞÓÇã ÇáãæÌæÏÉ áÏíß æ ÓæÝ íÊÛíÑ ÊÑÊíÈåÇ Ýí ÕÝÍÉ ÇáÈÏÇíÉ.ááÞíÇã ÈÐáß Þã ÈÊÛííÑ ÇÑÞÇã ÇáÇÞÓÇã æ ÓæÝ íßæä ÇáÊÑÊíÈ ÇáÌÏíÏ Úáì ÇáæÌå ÇáÊÇáí ÇáÞÓã ÑÞã 1 Ýí ÇáÈÏÇíÉ æ åßÐÇ . åÐå ÇáÎÇÕíÉ áä ÊÄËÑ Úáì ÇáÇÞÓÇã ÇáÏÇÎáíÉ.";
+$str[rcatdone] = "áÞÏ Êã ÇÚÇÏÉ ÊÑÊíÈ ÇáÇÞÓÇã!";
+$str[custominfo] = "Êãßäß åÐå ÇáÎÇÕíÉ ãä ÇÖÇÝÉ ãÚáæãÇÊ ÇßËÑ Úä ãáÝÇÊß ãËá ÇÐÇ ÇÑÏÊ Çä ÊÈíä ÍÌã ÇáãáÝ ßá ãÇ Úáíß åæ ÇäÊ ÊÖíÝ ÎÇäÉ ÌÏíÏÉ ÈÇÓã ÍÌã ÇáãáÝ æ ÊÐåÈ Çáì ÎÇäÉ ÊÍÑíÑ ÇáãáÝ Çæ ÇÖÇÝÉ ãáÝ ÌÏíÏ æ ÓæÝ ÊÌÏ Çä ÇáÎÇäÉ ÇáÌÏíÏ ÇÖíÝÊ æ ãÇ Úáíß Óæì ÊÚÈÆÊåÇ.";
+$str[afield] = "ÇÖÝ ÎÇäÉ";
+$str[efield] = "ÍÑÑ ÎÇäÉ";
+$str[dfield] = "ÇÍÐÝ ÎÇäÉ";
+$str[fieldname] = "ÇÓã ÇáÎÇäÉ";
+$str[fieldnameinfo] = "ÇßÊÈ åäÇ ÇÓã ÇáÎÇäÉ ÇáÊí ÊÑíÏ ÇÖÇÝÊåÇ ãËáÇ .. 'ÍÌã ÇáãáÝ'";
+$str[fielddesc] = "æÕÝ ÇáÎÇäÉ";
+$str[fielddescinfo] = "ÇßÊÈ åäÇ ÔÑÍ ááÎÇäÉ ÇáÊí ÊÑíÏ ÇÖÇÝÊåÇ ÍÊì íÝåã ÇáãÓÊÎÏã ãÇÐÇ ÊÚäí ãËáÇ .. 'ÍÌã ÇáãáÝ ÈÇáßíáæ ÈÇíÊ'";
+$str[fieldadded] = "Êã ÇÖÇÝÉ ÎÇäÉ ÌÏíÏÉ ÈÇÓã , <i>$form[name]</i> ÈäÌÇÍ";
+$str[fieldedited] = "Êã ÊÍÑíÑ ÇáÎÇäÉ ÇáãÓãÇå, <i>$form[name]</i> ÈäÌÇÍ";
+$str[dfielderror] = "áã ÈÇÎÊíÇÑ Çí ÎÇäÉ áÍÐÝåÇ";
+$str[fieldsdel] = "áÞÏ Êã ÍÐÝ ÇáÎÇäÉ ÇáãÎÊÇÑÉ ÈäÌÇÍ";
+$str[fmanageinfo] = "ãä åäÇ ÊÓÊØíÚ ÇáÊÍßã Ýí ãáÝÇÊ ÇáÈÇÝÇíá 3 ÇáãÚÑÈ Çæ ÇáãáÝÇÊ ÇáÊí ÊæÏ Çä Êßæä ãæÌæÏÉ Ýí ÇáÈÑäÇãÌ ¡ ÊÓÊØíÚ ÇÖÇÝÉ Çæ ÍÐÝ Çæ ÊÚÏíá Çí ãáÝ ÊÑíÏ ãä åäÇ.<br><b>ãáÇÍÙÉ:</b> áÇÓåá ÊÚÇãá ãÚ åÐå ÇáÎÇÕíÉ ÓÌá ÏÎæáß Çáì ÇáÈÑäÇãÌ Çáì áæÍÉ ÇáÊÍßã æ ÇÐåÈ Çáì ÕÝÍÉ ÇáãÚáæãÇÊ ÇáÎÇÕÉ ÈÇáÈÑäÇãÌ æ ßÃäß ÊÑíÏ ÊÍãíáå æ ÓæÝ ÊÌÏ ÇÑÊÈÇØÇÊ ÓÑíÚÉ áÍÐÝ Çæ ÊÚÏíá ÇáãáÝ.";
+$str[afile] = "ÇÖÝ ãáÝ";
+$str[efile] = "ÍÑÑ ãáÝ";
+$str[dfile] = "ÇÍÐÝ ãáÝ";
+$str[upload] = "ÇÑÝÚ ÇáãáÝ";
+$str[uploadinfo] = "ÇÑÝÚ åÐÇ ÇáãáÝ";
+$str[uploaderror] = "íæÌÏ ãáÝ ÇÎÑ ÈäÝÓ åÐÇ ÇáÇÓã $userfile_name ! ÇáÑÌÇÁ ÛíÑ ÇÓã ÇáãáÝ æ ÍÇæá ãÑÉ ÇÎÑì.";
+$str[uploaddone] = "áÞÏ Êã ÑÝÚ  åÐÇ ÇáãáÝ ÈäÌÇÍ $userfile_name ! æ ÚäæÇä ÑÇÈØ ÇáãáÝ Úáì ÇáÇäÊÑäÊ åæ";
+$str[uploaddone2] = "ÇÖÛØ åäÇ áÇÖÇÝÉ åÐÇ ÇáãáÝ Ýí ãßÊÈÉ ÇáÈÑÇãÌ ÇáÎÇÕÉ Èß.";
+$str[filename] = "ÇÓã ÇáãáÝ";
+$str[filenameinfo] = "åÐÇ åæ ÇÓã ÇáãáÝ ÇáÐí ÞãÊ ÈÇÖÇÝÊå, ãËá 'ÇáÈÇÝÇíá 3 ÇáãÚÑÈ' Çæ 'ÕæÑí.'";
+$str[filesd] = "ÇáæÕÝ ÇáÞÕíÑ";
+$str[filesdinfo] = "åäÇ ÊßÊÈ æÕÝÇ ÞÕíÑÇ ááãáÝ";
+$str[fileld] = "ÇáæÕÝ ÇáãÝÕá";
+$str[fileldinfo] = "ÇßÊÈ åäÇ ÇáæÕÝ ÇáãÝÕá ááãáÝ.";
+$str[filecreator] = "ãáßíÉ ÇáÈÑäÇãÌ";
+$str[filecreatorinfo] = "åäÇ ÇßÊÈ ÕÇÍÈ ÇáãáÝ æ ÇáÐí ÊÚæÏ ÍÞæÞ ÇáãáÝ Çáíå.";
+$str[fileversion] = "ÑÞã ÇáÇÕÏÇÑÉ";
+$str[fileversioninfo] = "ÇßÊÈ åäÇ ÑÞã ÇáÇÕÏÇÑÉ ÇáÎÇÕ ÈÇáãáÝ ãËá  3.0 Çæ 1.3 Beta";
+$str[filess] = "ÕæÑÉ Úä ÇáãáÝ";
+$str[filessinfo] = "ÖÚ ÚäæÇä ÕæÑÉ ááÈÑäÇãÌ Ýí ÍÇáÉ ÇáÚãá.";
+$str[filedocs] = "ÚäæÇä ãáÝ ÇáÊÚáíãÇÊ";
+$str[filedocsinfo] = "ÇßÊÈ åäÇ ÚäæÇä ãáÝ ÇáÊÚáíãÇÊ ÇáÎÇÕ ÈÇáãáÝ ";
+$str[fileurl] = "ÚäæÇä ÇáãáÝ Úáì ÇáÇäÊÑäÊ";
+$str[fileurlinfo] = "ÇßÊÈ åäÇ ÚäæÇä ÇáÑÇÈØ Çæ ÇáæÕáÉ ÇáÊí íÓÊØíÚ ÇáÒÇÆÑ ÊÍãíá ÇáãáÝ ãä ÎáÇáåÇ. <a href=\"javascript:NewWindow('pafiledb.php?action=admin&ad=file&file=upload','fileupload','600','450','custom','front');\">Çæ ÇÑÝÚ ÇáãáÝ ÇÐÇ áã íßä áå ÑÇÈØ ãä Úáì ÌåÇÒß</a>";
+$str[filepi] = "ÕæÑÉ ãÕÇÍÈÉ";
+$str[filepiinfo] = "åÐå ÇáÕæÑÉ ÓæÝ ÊÕÇÍÈ ÇÓã ÇáãáÝ Ýí ÞÇÆãÉ ÇáãáÝÇÊ Ýí ÇáÞÓã.";
+$str[filecat] = "ÇáÞÓã";
+$str[filecatinfo] = "ÇÎÊÑ ÇáÕíã ÇáÐí ÊÑíÏ ÇÖÇÝÉ ÇáãáÝ Çáíå.";
+$str[filelicense] = "ÇÊÝÇÞíÉ ÇáãáÝ";
+$str[filelicenseinfo] = "ÇÎÊÑ ãä åäÇ ÇÍÏ ÇáÇÊÝÇÞíÇÊ ÇáÊí ÞãÊ ÈÇäÔÇÆåÇ æ ÓæÝ ÊÙåÑ åÐå ÇáÇÊÝÇÞíÉ ÞÈá ÊÍãíá ÇáãáÝ.";
+$str[filepin] = "ÊËÈíÊ ÇáãáÝ";
+$str[filepininfo] = "ÇÐÇ ßäÊ ÊÚÊÞÏ Çä åÐÇ ÇáãáÝ Çæ ÇáÈÑäÇãÌ ãåã ÌÏÇ ÈÇáäÓÈÉ áÚãæã ÇáãÓÊÎÏãíä ÝËÈÊå ãä åäÇ æ ÓæÝ íÙåÑ Úáì ÑÃÓ ÞÇÆãÉ ÇáãáÝÇÊ Ýí ÇáÞÓã ÇáÎÇÕ Èå æ áä íäÒá ÍÊì ÈÅÖÇÝÉ ãáÝÇÊ ÌÏíÏÉ.";
+$str[fileadded] = "áÞÏ ÊãÊ ÇÖÇÝÉ ÇáãáÝ ÇáÌÏíÏ ÈÇÓã, <i>$form[name]</i> ÈäÌÇÍ!";
+$str[fileedited] = "áÞÏ Êã ÊÍÑíÑ ÇáãáÝ ÇáÊÇáí, <i>$form[name]</i> ÈäÌÇÍ!";
+$str[fderror] = "áã ÊÞã ÈÇÎÊíÇÑ Çí ãáÝ áÍÐÝå!";
+//You have reached Line 200. PHP Arena is not responsible for deaths related to sitting in front of your computer for a long time while translating this file.
+$str[filesdeleted] = "áÞÏ Êã ÍÐÝ ÇáãáÝ / ÇáãáÝÇÊ ÇáÊí ÞãÊ ÈÇÎÊíÇÑåÇ ÈäÌÇÍ!";
+$str[lmanageinfo] = "ÊÓÊØíÚ ÇÓÊÎÏÇã ÎÇÕíÉ ÇáÇÊÝÇÞíÇÊ æ ÇáÊÑÇÎíÕ ÇáãæÌæÏÉ Ýí áæÍÉ ÇáÊÍßã ¡ ßãÇ ÊÓÊØíÚ ÇíÖÇ ÊÍÑíÑ Çæ ÍÐÝ Çí ÇÊÝÇÞíÉ ãæÌæÏÉ . íÊã ÇÎÊíÇÑ ÇáÇÊÝÇÞíÉ ãä ÊÍÑíÑ Çæ ÇÖÇÝÉ ãáÝ ÌÏíÏ. æ åÐå ÇáÇÊÝÇÞíÉ ÓæÝ ÊÙåÑ ááãÓÊÎÏã ÞÈá ÊÍãíá ÇáãáÝ æ áä íÊã ÇáÊÍãíá ÇáÇ ÈÇáãæÇÝÞÉ ÚáíåÇ.";
+$str[alicense] = "ÇÖÝ ÇÊÝÇÞíÉ";
+$str[elicense] = "ÍÑÑ ÇÊÝÇÞíÉ";
+$str[dlicense] = "ÇÍÐÝ ÇÊÝÇÞíÉ";
+$str[lname] = "ÇÓã ÇáÇÊÝÇÞíÉ";
+$str[ltext] = "äÕ ÇáÇÊÝÇÞíÉ";
+$str[licenseadded] = "áÞÏ Êã ÇÖÇÝÉ ÇáÇÊÝÇÞíÉ ÇáÌÏíÏÉ ÈÇÓã, <i>$form[name]</i> ÈäÌÇÍ!";
+$str[licenseedited] = "áÞÏ Êã ÊÍÑíÑ ÇáÇÊÝÇÞíÉ ÇáÊÇáíÉ, <i>$form[name]</i> ÈäÌÇÍ!";
+$str[lderror] = "áã ÊÞã ÈÇÎÊíÇÑ Çí ãä ÇáÇÊÝÇÞíÇÊ ÇáãæÌæÏÉ áÍÐÝåÇ!";
+$str[ldeleted] = "áÞÏ Êã ÍÐÝ ÇáÇÊÝÇÞíÉ / ÇáÇÊÝÇÞíÇÊ ÇáÊí ÇÎÊÑÊåÇ ÈäÌÇÍ.";
+$str[login] = "ÊÓÌíá ÇáÏÎæá";
+$str[username] = "ÇÓã ÇáãÓÊÍÏã";
+$str[password] = "ßáãÉ ÇáãÑæÑ";
+$str[loggedin] = "áÞÏ Êã ÊÓÌíá ÏÎæáß . <a href=\"pafiledb.php?action=admin&ad=main\">ÇÖÛØ åäÇ</a> ááÇÓÊãÑÇÑ.";
+$str[loginerror] = "ÎØÃ Ýí ÇÓã ÇáãÓÊÎÏã Çæ ßáãÉ ÇáãÑæÑ!";
+$str[loggedout] = "áÞÏ Êã ÊÓÌíá ÎÑæÌß.";
+$str[changeemail] = "ÊÛííÑ ÇáÈÑíÏ ÇáÇáíßÊÑæäí";
+$str[emailad] = "ÇáÈÑíÏ ÇáÇáíßÊÑæäí";
+$str[confpass] = "ÊÃßíÏ ßáãÉ ÇáãÑæÑ";
+$str[nopermission] = "ÚÝæÇ áíÓ áÏíß ÇáÊÕÑíÍ ÇááÇÒã áÇÓÊÎÏÇã åÐÇ ÇáÞÓã.";
+$str[emailchanged] = "áÞÏ íã ÊÛííÑ ÈÑíÏß ÇáÇáíßÊÑæäí ÈäÌÇÍ!";
+$str[changepasserror] = "Çä ßáãÉ ÇáãÑæÑ æ ÇáÊÃßíÏ ÇáÎÇÕ ÈåÇ áíÓÇ ãÊØÇÈÞíä. äÑÌæ ÇáÑÌæÚ æ ÇáãÍÇæáÉ ãÑÉ ÇÎÑì";
+$str[yourpasschanged] = "áÞÏ Êã ÊÛííÑ ßáãÉ ÇáãÑæÑ ÇáÎÇÕÉ Èß ÈäÌÇÍ!";
+$str[dbname] = "ÇÓã ãßÊÈÉ ÇáÈÑÇãÌ Çæ ÇáãáÝÇÊ";
+$str[dbnameinfo] = "åÐÇ ÇáÇÓã ÇáÐí ÓÊÚÑÝ Èå ÇáãßÊÈÉ ßßá ãËá 'ãßÊÈÉ ÇáÈÑÇãÌ ÇáãÌÇäíÉ'";
+$str[dbdesc] = "æÕÝ áãßÊÈÉ ÇáÈÑÇãÌ";
+$str[dbdescinfo] = "æÕÝ áãßÊÈÉ ÇáÈÑÇãÌ .. ãËá : ãßÊÈÉ ÇáÈÑÇãÌ ÇáãÌÇäíÉ áÈÑÇãÌ ÇáãÍÏËÉ Çæ ÛíÑåÇ";
+$str[dburl] = "ÚäæÇä ãßÊÈÉ ÇáÈÑÇãÌ";
+$str[dburlinfo] = "ÇßÊÈ åäÇ ÚäæÇä ÇáãÌáÏ ÇáÑÆíÓí ááÈÇÝÇíá 3 ÇáãÚÑÈ ";
+$str[topnum] = "ÚÏÏ ÇÝÖá ÇáãáÝÇÊ ÇáÊí ÊæÏ ";
+$str[topnuminfo] = "åÐÇ ÇáÚÏÏ íÚäí ßã ÚÏÏ ÇáãáÝÇÊ ÇáÊí ÊæÏ ÙåæÑåÇ Ýí ÇÝÖá ÇáãáÝÇÊ ÊÍãíáÇ";
+$str[hpurl] = "ÚäæÇä ãæÞÚß";
+$str[hpurlinfo] = "åÐÇ åæ ÚäæÇä ÕÝÍÉ ÇáÈÏÇíÉ áãæÞÚß";
+$str[nfdays] = "ãÏÉ ÇáãáÝÇÊ ÇáÌÏíÏÉ";
+$str[nfdaysinfo] = "ßã ÇáãÏÉ ÇáÊí ÊÑíÏ ÇáãáÝ Çä íÈÞì Öãä ÇáãáÝÇÊ ÇáÌÏíÏÉ ãËáÇ  ÎãÓÉ ÇíÇã Çä ÇáãáÝ ÓæÝ íÈÞì Ýí ÞÇÆãÉ ÇáãáÝÇÊ ÇáÌÏíÏÉ áãÏÉ ÎãÓÉ ÇíÇã .";
+$str[timeoffset] = "ÝÇÑÞ ÇáÊæÞíÊ";
+$str[timeoffsetinfo] = "ÝÇÑÞ ÇáÊæÞíÊ Úä ãÏíäÉ ÌÑíäÊÔ";
+$str[tz] = "ÈÊæÞíÊ";
+$str[tzinfo] = "ÇßÊÈ ÇÓã ÇáãÏíäÉ ÇáÊí ÊÑíÏ Çä ÊÙåÑ ÇáÇæÞÇÊ ÈÊæÞíÊåÇ ãËá : ÈÊæÞíÊ ÌÑíäÊÔ";
+$str[header] = "ÇáåíÏÑ";
+$str[headerinfo] = "åÐå ÇáÕÝÍÉ åí ÇáÊí ÓÊÙåÑ Úáì ÑÃÓ ãßÊÈÉ ÇáÈÑÇãÌ";
+$str[footer] = "ÇáÝæÊÑ";
+$str[footerinfo] = "åÐå ÇáÕÝÍÉ åí ÇáÊí ÓÊÙåÑ Ýí ÇÓÝ ãßÊÈÉ ÇáÈÑÇãÌ.";
+$str[styleset] = "ÇáãÙåÑ ÇáÎÇÑÌí ááãßÊÈÉ";
+$str[stylesetinfo] = "ÇÎÊÑ ÇáÔßá ÇáÐí ÊÑíÏ æ ßãÇ Çäå ÊÓÊØíÚ ÕäÚ ÇáÔßá ÇáÐí ÊÑíÏ æ Úáíß æÖÚå Ýí ãÌáÏ styles";
+$str[stats2] = "ÇÙåÇÑ ÇáãÚáæãÇÊ";
+$str[statsinfo] = "ÇÎÊÑ åäÇ ÇÐÇ ÇÑÏÊ Çä íÙåÑ ÇÓÝ ÇÞÓÇã ÇáãßÊÈÉ ãÚáæãÇÊ Úä ÞÇÚÏÉ ÇáÈíÇäÇÊ .";
+$str[settingschanged] = "áÞÏ Êã ÊÍÏíË ãÚáæãÇÊ ãßÊÈÉ ÇáÈÑÇãÌ ÈäÌÇÍ!";
+$str[lang] = "áÛÉ ÇáÈÑäÇãÌ";
+$str[langinfo] = "ÇÎÊÑ ÇááÛÉ ÇáÊí ÊÉÏ Çä íÙåÑ ÇáÈÇÝÇíá 3 ÈåÇ .";
+
+//Start Beta 2.1 Language File
+$str[settings] = "ÇáÇÚÏÇÏÇÊ";
+$str[pafdbsettings] = "ÇÚÏÇÏÊ ÇáÈÇÝÇíá 3 ÇáãÚÑÈ";
+
+//Start Beta 3 Language File 
+$str[sortby] = "ÊÑÊíÈ ÍÓÈ"; 
+$str[sort] = "ÊÑÊíÈ"; 
+$str[name] = "ÇáÇÓã"; 
+$str[bdb] = "äÓÎÉ ÇÍÊíÇØíÉ ãä ÞÇÚÏÉ ÇáÈíÇäÇÊ"; 
+$str[rdb] = "ÊÍÏíË ÞÇÚÏÉ ÇáÈíÇäÇÊ"; 
+$str[rdbinfo] = "ÊÓÊØíÚ ÇÓÊÚÇÏÉ æ ÊÍÏíË ÞÇÚÏÉ ÇáÈíÇäÇÊ ÈÇáäÓÎÉ ÇáÇÍÊíÇØíÉ ÇáÊí ÓÈÞ æ Çä ÇÎÐÊåÇ ãä ÞÇÚÏÉ ÇáÈíÇäÇÊ. <b>ãáÇÍÙÉ:</b> åÐå ÇáÎØæÉ ÓæÝ ÊÍÐÝ ßÇÝÉ ÇáãÚáæãÇÊ ÇáãæÌæÏÉ Ýí ÞÇÚÏÉ ÇáÈíÇäÇÊ æ ÇáÊí ÊãÊ ÈÚÏ ÇÎÑ ÍÝÙ äÓÎÉ ÇÍÊíÇØíÉ ááÞÇÚÏÉ!<p>ÇÎÊÑ ÇáãáÝ áÊÍÏíËå:"; 
+$str[rdbdone] = "áÞÏ Êã ÊÍÏíË ÞÇÚÉ ÇáÈíÇäÇÊ ÈäÌÇÍ! ÇÐÇ ßÇäÊ ãÚáæãÇÊ ÇáãÔÑÝ Ýí ÞÇÚÏÉ ÇáÈíÇäÇÊ ÞÏíãÉ ßá ãÇ Úáíß åæ ÇáÈÍË Ýí ÇáäÓÎ ÇáÇÍÊíÇØíÉ ÇáãÊæÝÑÉ áÏíß æ ÇáÊÍÏíË áÇÎÑ äÓÎÉ ÇÍÊíÇØíÉ ãæÌæÏÉ áÏíß ¡ ÑÈãÇ íÊØáÈ ãäß ÈÚÏ åÐÇ ÊÓÌíá ÇáÏÎæá ãÑÉ ÇÎÑì ."; 
+$str[home] = "ÕÝÍÉ ÇáÈÏÇíÉ"; 
+?>

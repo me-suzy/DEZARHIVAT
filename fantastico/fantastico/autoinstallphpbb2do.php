07f5c425e0487dbf66b2bb00c9dd3c632f6df153
me-suzy <me.suzana@gmail.com>
2025-08-10 21:51:49 +0300

    Add fantastico/fantastico/autoinstallphpbb2do.php

---
+++ fantastico/fantastico/autoinstallphpbb2do.php
@@ -0,0 +1,89 @@
+<cpanel include="fantasticoheader.php">
+<table width=100% class="TableMiddle">
+<tr> 
+<td>
+<p class="TableMiddleHead">Install phpBB2 (2/2)</p>
+<!--
+<cpanel Mysql="adddb($FORM{'db'})"> 
+<cpanel Mysql="adduser($FORM{'dbuser'},$FORM{'password'})">
+<cpanel Mysql="adduserdb($FORM{'connectdb'},$FORM{'connectuser'})">
+-->
+<?
+$MYSQLUSER = $connectuser;
+$MYSQLDB = $connectdb;
+$MYSQLPASS = $password;
+$USER = "<cpanel print="$user">";
+$DOMAIN = "<cpanel print="DOMAIN">";
+$USER_ROOT = "<cpanel print="$homedir">/public_html/";
+$SCRIPTPATH = "<cpanel print="$homedir">/public_html/forum/";
+$DEPOT = "/var/autoinstall/english/forum";
+$SQLDATA = $SCRIPTPATH."data.sql";
+$ADMIN = $admin;
+$ADMINPASS = md5($password);
+$SITENAME = $sitename;
+$SMALLDESCRIPTION = $smalldescription;
+$EMAILSIGNATURE = $emailsignature;
+$ADMINEMAIL = $adminemail;
+$LANGUAGE = $language;
+
+//-----------------------------------------
+
+if(!isset($USER)){
+	die("<hr><b>No User defined!</b><hr>");
+}
+if(!isset($DEPOT)){
+	die("<hr><b>No Depot defined!</b><hr>");
+}
+	$shell="cp -rfp ". $DEPOT ." ". $USER_ROOT;
+	system($shell);
+
+$CONFIG_FILE[]=$SCRIPTPATH."config.php";
+$CONFIG_FILE[]=$SQLDATA;
+
+foreach($CONFIG_FILE AS $FILE){
+	if(is_file($FILE)){
+		echo "<b>".$FILE."</b> configured<br>";
+		$fd = fopen ($FILE, "r");
+		$contents = fread ($fd, filesize ($FILE));
+		$contents = str_replace("<MYSQLUSER>", $MYSQLUSER, $contents);
+		$contents = str_replace("<MYSQLDB>", $MYSQLDB, $contents);
+		$contents = str_replace("<MYSQLPASS>", $MYSQLPASS, $contents);		
+		$contents = str_replace("<USER>", $USER, $contents);
+		$contents = str_replace("<DOMAIN>", $DOMAIN, $contents);
+		$contents = str_replace("<ADMIN>", $ADMIN, $contents);
+		$contents = str_replace("<ADMINPASS>", $ADMINPASS, $contents);
+		$contents = str_replace("<SITENAME>", $SITENAME, $contents);
+		$contents = str_replace("<ADMINEMAIL>", $ADMINEMAIL, $contents);
+		$contents = str_replace("<SMALLDESCRIPTION>", $SMALLDESCRIPTION, $contents);
+		$contents = str_replace("<EMAILSIGNATURE>", $EMAILSIGNATURE, $contents);
+		$contents = str_replace("<LANGUAGE>", $LANGUAGE, $contents);
+
+		fclose ($fd);
+		$fd = fopen ($FILE, "w");
+		fputs($fd, $contents);
+		fclose($fd);
+	}else{
+echo ++$i.") File:<b> ".$FILE."</b> [<span class='Hint'><i>Error </i></span>]<br>";
+	}
+}
+	$shellsqlimport="mysql -u ". $MYSQLUSER ." -p". $MYSQLPASS." ".$MYSQLDB." < $SQLDATA";
+	system($shellsqlimport);
+?>
+</td>
+</tr>
+               
+            
+</table>
+<p align="center"><b>Please notice:</b></p>
+<p align="left">We only offer auto-installation and auto-configuration of <span class="Emphasize">phpBB2</span> but do not offer any kind of support. If you need support, please visit the <a href="http://phpbb.com/" target="_blank">Support Forum</a>.</p>
+<p align="left">Now you can enter the admin area and setup <span class="Emphasize">phpBB2</span>.</p>
+<p class="BoldText" align="center">URL info:</p>
+<p align="left">You need a username and a password to enter the admin area. Your username is <span class="Hint"><b>admin</b></span>. Your password is <span class="Hint"><b><cpanel print="$FORM{'password'}"></b></span>. The full URL to the admin area <span class="Hint"><b>(Bookmark this!)</b></span>: <a href="http://<cpanel print="DOMAIN">/forum/admin/" target="blank">http://<cpanel print="DOMAIN">/forum/admin/</a></p>
+<p align="left">The full URL to <span class="Emphasize">phpBB2</span>: <a href="http://<cpanel print="DOMAIN">/forum/" target="blank">http://<cpanel print="DOMAIN">/forum/</a></p>
+<p>&nbsp;</p>
+              
+<tr align="center"> 
+                
+<td><a href="index.php">Fantastico Auto-installer overview</a>
+
+<cpanel include="fantasticofooter.php">

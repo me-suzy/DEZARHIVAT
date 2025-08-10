9d1cc80e3a2f5f9fc26c179160abb0d8a04b6aa3
me-suzy <me.suzana@gmail.com>
2025-08-11 01:01:51 +0300

    Add sitesupra2rc1/supra/language/english/edtext.php

---
+++ sitesupra2rc1/supra/language/english/edtext.php
@@ -0,0 +1,10 @@
+<?
+$__=Array(
+'Text Editor'					=>'Super-Puper Editor'			,
+'Change selected text to upper case'		=>'Âåðõíèé ðåãèñòð âûäåëåííîãî òåêñòà'	,
+'Change selected text to lower case'		=>'Íèæíèé ðåãèñòð âûäåëåííîãî òåêñòà'	,
+'Capitalize first character of each word'	=>'Çàãëàâíàÿ ïåðâàÿ áóêâà êàæäîãî ñëîâà',
+'Invert case of selected string'		=>'Èíâåðòèðîâàòü ðåãèñòð'		,
+'Toggle Word Wrap On/Off'			=>'Ïåðåíîñ ñòðîê Âêë/Âûêë'		,
+);
+?>

e6fca64405d3a7a3f04c715f30cf96b6584a3bef
me-suzy <me.suzana@gmail.com>
2025-08-10 23:23:00 +0300

    Add pafiledb_3.0-final/pafiledb_3.0-final/admin.php

---
+++ pafiledb_3.0-final/pafiledb_3.0-final/admin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+  paFileDB 3.0
+  ©2001/2002 PHP Arena
+  Written by Todd
+  todd@phparena.net
+  http://www.phparena.net
+  Keep all copyright links on the script visible
+  Please read the license included with this script for more information.
+*/
+header("Location: pafiledb.php?action=admin");
+?>

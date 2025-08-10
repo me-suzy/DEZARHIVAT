151df96446c24d650d4d3ffac583a3d785617687
me-suzy <me.suzana@gmail.com>
2025-08-10 21:52:11 +0300

    Add fantastico/fantastico/autoinstallwebsitedo.php

---
+++ fantastico/fantastico/autoinstallwebsitedo.php
@@ -0,0 +1,98 @@
+<cpanel include="fantasticoheader.php">
+
+            
+<table width=100% class="TableMiddle">
+              
+<tr> 
+                
+<td>
+<p class="TableMiddleHead">Install phpWebSite (2/2)</p>
+<!--
+<cpanel Mysql="adddb($FORM{'db'})"> 
+<cpanel Mysql="adduser($FORM{'dbuser'},$FORM{'password'})">
+<cpanel Mysql="adduserdb($FORM{'connectdb'},$FORM{'connectuser'})">
+-->
+
+<?
+$MYSQLUSER = str_replace("-","",$connectuser);
+$MYSQLDB = str_replace("-","",$connectdb);
+$MYSQLPASS = $password;
+$USER = "<cpanel print="$user">";
+$DOMAIN = "<cpanel print="DOMAIN">";
+$USER_ROOT = "<cpanel print="$homedir">/public_html/";
+$SCRIPTPATH = "<cpanel print="$homedir">/public_html/website/";
+$DEPOT = "/var/autoinstall/english/website";
+$SQLDATA = $SCRIPTPATH."website.sql";
+$ADMIN = "admin";
+$ADMINPASS = md5($password);
+$SITENAME = $sitename;
+$SITEURL = $siteurl;
+$TITLE = $title;
+$SUBTITLE = $subtitle;
+$BEGINDATE = $begindate;
+$ADMINEMAIL = $adminemail;
+$BACKENDTITLE = $backendtitle;
+//-----------------------------------------
+
+if(!isset($USER)){
+	die("<hr><b>No User defined!</b><hr>");
+}
+if(!isset($DEPOT)){
+	die("<hr><b>No Depot defined!</b><hr>");
+}
+	$shell="cp -rfp ". $DEPOT ." ". $USER_ROOT;
+	system($shell);
+
+
+$CONFIG_FILE[]=$SCRIPTPATH."config.php";
+$CONFIG_FILE[]=$SCRIPTPATH."website.sql";
+
+foreach($CONFIG_FILE AS $FILE){
+	if(is_file($FILE)){
+		echo "<b>".$FILE."</b> configured<br>";
+		$fd = fopen ($FILE, "r");
+		$contents = fread ($fd, filesize ($FILE));
+		$contents = str_replace("<MYSQLUSER>", $MYSQLUSER, $contents);
+		$contents = str_replace("<MYSQLDB>", $MYSQLDB, $contents);
+		$contents = str_replace("<MYSQLPASS>", $MYSQLPASS, $contents);		
+		$contents = str_replace("<USER>", $USER, $contents);
+		$contents = str_replace("<DOMAIN>", $DOMAIN, $contents);
+		$contents = str_replace("<ADMIN>", $ADMIN, $contents);
+		$contents = str_replace("<SITENAME>", $SITENAME, $contents);
+		$contents = str_replace("<SITEURL>", $SITEURL, $contents);
+		$contents = str_replace("<TITLE>", $TITLE, $contents);
+		$contents = str_replace("<SUBTITLE>", $SUBTITLE, $contents);
+		$contents = str_replace("<BEGINDATE>", $BEGINDATE, $contents);
+		$contents = str_replace("<ADMINEMAIL>", $ADMINEMAIL, $contents);
+		$contents = str_replace("<BACKENDTITLE>", $BACKENDTITLE, $contents);		
+		$contents = str_replace("<ADMINPASS>", $ADMINPASS, $contents);
+
+		fclose ($fd);
+		$fd = fopen ($FILE, "w");
+		fputs($fd, $contents);
+		fclose($fd);
+	}else{
+echo ++$i.") File:<b> ".$FILE."</b> [<span class='Hint'><i>Error </i></span>]<br>";
+	}
+}
+	$shellsqlimport="mysql -u ". $MYSQLUSER ." -p". $MYSQLPASS." ".$MYSQLDB." < $SQLDATA";
+	system($shellsqlimport);
+?>
+</td>
+</tr>
+               
+            
+</table>
+<p align="center"><b>Please notice:</b></p>
+<p align="left">We only offer auto-installation and auto-configuration of <span class="Emphasize">phpWebSite</span> but do not offer any kind of support. If you need support, please visit the <a href="http://sourceforge.net/forum/?group_id=15539" target="_blank">Support Forum</a>.</p>
+<p align="left">Now you can enter the admin area and setup <span class="Emphasize">phpWebsite</span>.</p>
+<p class="BoldText" align="center">URL info:</p>
+<p align="left">You need a username and a password to enter the admin area. Your username is <span class="Hint"><b>admin</b></span>. Your password is <span class="Hint"><b><cpanel print="$FORM{'password'}"></b></span>. The full URL to the admin area <span class="Hint"><b>(Bookmark this!)</b></span>: <a href="http://<cpanel print="DOMAIN">/website/admin.php" target="blank">http://<cpanel print="DOMAIN">/website/admin.php</a></p>
+<p align="left">The full URL to phpWebsite: <a href="http://<cpanel print="DOMAIN">/website/" target="blank">http://<cpanel print="DOMAIN">/website/</a></p>
+<p>&nbsp;</p>
+              
+<tr align="center"> 
+                
+<td><a href="index.php">Fantastico Auto-installer overview</a>
+
+<cpanel include="fantasticofooter.php">

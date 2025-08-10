32f416927de05862d29b559029a3111646d060fe
me-suzy <me.suzana@gmail.com>
2025-08-11 00:58:49 +0300

    Add siteseed-v1.4-r8-20020924/siteseed-20020924/siteseed/include/db_connect.php

---
+++ siteseed-v1.4-r8-20020924/siteseed-20020924/siteseed/include/db_connect.php
@@ -0,0 +1,40 @@
+<?
+/**************************************
+
+Project: Siteseed (copyright MrNet 2001 - All right reserved)
+Filename: include/db_connect.php
+Last modified: 20013103
+
+***************************************/
+
+require_once "config.php";
+require "bo/include/error.php";
+
+if (! DB_HOST) error ("please define DB_HOST");
+
+// if we cant connect to mysql host
+if (! mysql_pconnect (DB_HOST, DB_USER, DB_PASSWORD)) 
+{
+	error (mysql_error());
+}
+
+if (! DB_NAME) error ("You must define DB_NAME");
+
+
+// lets try to find DB_NAME
+$query = mysql_list_dbs();
+
+for ($x = 0; $x <= mysql_num_rows($query)-1; $x++) 
+{
+	$db_name = mysql_tablename($query, $x);
+	
+	if ($db_name == DB_NAME) $found_db = 1;
+}
+
+
+// lets try to select the db
+if (! mysql_select_db (DB_NAME))
+{
+	error (mysql_error()."\nwhile selecting database");
+}
+?>

2b345e844e251b675aa69fba842079d8de060b6c
me-suzy <me.suzana@gmail.com>
2025-08-10 21:51:51 +0300

    Add fantastico/fantastico/autoinstallphpbb2home.php

---
+++ fantastico/fantastico/autoinstallphpbb2home.php
@@ -0,0 +1,48 @@
+<cpanel include="fantasticoheader.php">
+            
+			    
+<p align="center"><img src="images/phpbb2.gif" width="200" height="91">
+<p>
+<table width="100%" class="MainMenuItems" align="center">
+<tr> 
+<td align="center" class="BoldText">&nbsp;</td>
+</tr>
+<tr>
+<td class="linieuntenaligncenter">
+<div align='left'>
+<p>&nbsp;
+<p>
+<a href='http://<cpanel print="DOMAIN">/forum/' target='_blank'>Enter phpBB2</a>
+<p>
+<a href='http://<cpanel print="DOMAIN">/forum/admin/' target='_blank'>Enter admin area</a>
+<p>
+<a href='http://phpbb.com/' target='_blank'>Visit the phpBB2 support forum</a> <br>
+(We are not associated with the support forum)
+<p>&nbsp;
+<p class='Hint'><b>Remove phpBB2</b>
+<p>
+<p>There is a directory called <span class="BoldText">forum</span> in your web root directory. If this directory was created by you rather than by this auto-installer, then DO NOT attempt to remove it. If you click on &quot;Remove phpBB2&quot;, then the directory called <span class="BoldText">forum</span> and any existing MySQL database and MySQL user called <span class="BoldText"><?=str_replace("-","","<cpanel print="$user">_forum")?></span> will be removed.</p>
+<p class='Hint'>Notice! Clicking on &quot;Remove phpBB2&quot; will remove the data even if they were not created by this auto-installer.</p>
+<form action='autoinstallremoveconfirm.php'>
+<p>
+<input type='hidden' name='removefolder' value='<cpanel print="$homedir">/public_html/forum'>
+<input type='hidden' name='removeapp' value='phpBB2'>
+<input type='hidden' name='removeuser' value='<cpanel print="$user">_bbforum'>
+<input type='hidden' name='removedb' value='<cpanel print="$user">_bbforum'>
+</p>
+<p class="ButtonPosition">
+<input type='submit' name='Submit' value='Remove phpBB2' class='ButtonDef'>
+</p>
+</FORM>
+</div>
+</td>
+</tr>
+</table>
+<p>&nbsp;</p>
+<table width="100%" border="0" cellpadding="0">
+              
+<tr align="center"> 
+                
+<td><a href="index.php">Fantastico Auto-installer overview</a>
+
+<cpanel include="fantasticofooter.php">

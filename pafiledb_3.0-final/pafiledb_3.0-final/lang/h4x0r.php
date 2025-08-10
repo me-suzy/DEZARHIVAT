34d8b12f3455a567f69c132da9034bd6a84973f1
me-suzy <me.suzana@gmail.com>
2025-08-10 23:24:04 +0300

    Add pafiledb_3.0-final/pafiledb_3.0-final/lang/h4x0r.php

---
+++ pafiledb_3.0-final/pafiledb_3.0-final/lang/h4x0r.php
@@ -0,0 +1,266 @@
+<?php
+/*
+  paFileDB 3.0
+  ©2001 PHP Arena
+  Written by Todd
+  todd@phparena.uni.cc
+  http://www.phparena.uni.cc
+  Keep all copyright links on the script visible
+  Please read the license included with this script for more information.
+
+  ##########
+  #paFileDB 3.0 Language File
+  #h4x0r
+  #Translated by Todd
+  ##########
+
+  ##########	
+  #If you are translating this file, DO NOT translate anything in brackets, such as {something}, any variables ($something) or arrays #($something[somethingelse]), or HTML tags (<something></something>) or else parts of the script will not work
+  #Also, do not delete any lines in this script
+  ##########
+
+*/
+
+//Start Beta 2 Language File
+$str[time] = "4|| 71M35 4r3";
+$str[power] = "<font size=\"6\">j00 h4v3 b33n h4x0r3d!!!!!!!!!!!!!1111</font><p>P0W3r3d by";
+$str[exectime] = "3x3(u710N 71M3";
+$str[numqueries] = "My5Q| Qu3r135 U53d";
+$str[adminops] = "4dM1N 0p710N5";
+$str[mainpage] = "M41N P463";
+$str[addcat] = "4dd (47360ry";
+$str[editcat] = "3d17 (47360ry";
+$str[delcat] = "D3|373 (47360ry";
+$str[ordercat] = "R30rd3r (47360r135";
+$str[category] = "(47360ry";
+$str[files] = "F1|35";
+$str[jump] = "(47360ry JuMp";
+$str[logged] = "|0663d 1N 45";
+$str[admincenter] = "4dM1N (3N73r";
+$str[logout] = "|060u7";
+$str[file] = "F1|3";
+$str[date] = "D473 4dd3d";
+$str[rating] = "R471N6";
+$str[dls] = "D0WN|04d5";
+$str[email] = "3-M41| F1|3 70 4 Fr13Nd";
+$str[emailinfo] = "1f y0u W0u|d |1k3 4 fr13Nd 70 kN0W 4b0u7 7h15 f1|3, y0u (4N f1|| 0u7 4Nd 5ubM17 7h15 f0rM 4Nd 4N 3-M41| (0N741N1N6 7h3 f1|3'5 1Nf0rM4710N W1|| b3 3-M41|3d 70 y0ur fr13Nd!";
+$str[yname] = "Y0ur N4M3";
+$str[yemail] = "Y0ur 3-M41| 4ddr355";
+$str[fname] = "Fr13Nd'5 N4M3";
+$str[femail] = "Fr13Nd'5 3-M41| 4ddr355";
+$str[esub] = "3-M41| 5ubj3(7";
+$str[etext] = "3-M41| 73x7";
+$str[defaultmail] = "1 7h0u6h7 y0u M16h7 b3 1N73r3573d 1N d0WN|04d1N6 7h3 f1|3 |0(473d 47";
+$str[semail] = "53Nd 3-M41|";
+$str[msg] .= "h3||0 $friend[name],\n";
+$str[msg] .= "$email[message]\n\n";
+$str[msg] .= "----------\n";
+$str[msg] .= "7h15 3-M41| W45 53N7 7hr0u6h 7h3 \"{dbname}\" f1|3 d474b453. 7h3 W3bM4573r5 0f 7h3 \"{dbname}\" f1|3 d474b453 74k3 N0 r35p0N51b1|17y f0r 7h3 3-M41|5 53N7 7hr0u6h 7h3 d474b453";
+$str[econf] = "7h4Nk y0u! Y0ur 3-M41| h45 b33N 53N7 70 $friend[name]'5 3-M41| 4ddr355.";
+$str[editfile] = "3d17 F1|3";
+$str[deletefile] = "D3|373 F1|3";
+$str[desc] = "D35(r1p710N";
+$str[creator] = "(r3470r";
+$str[version] = "V3r510N";
+$str[scrsht] = "5(r33N5h07";
+$str[docs] = "D0(uM3N74710N";
+$str[lastdl] = "|457 D0WN|04d";
+$str[never] = "N3V3r";
+$str[votes] = "V0735";
+$str[download] = "D0WN|04d F1|3";
+$str[rate] = "R473 F1|3";
+$str[license] = "|1(3N53 46r33M3N7";
+$str[licensewarn] = "Y0u Mu57 46r33 70 7h15 |1(3N53 46r33M3N7 70 d0WN|04d";
+$str[iagree] = "1 46r33";
+$str[dontagree] = "1 D0N'7 46r33";
+$str[rerror] = "50rry, y0u h4V3 4|r34dy r473d 7h15 f1|3.";
+$str[rateinfo] = "Y0u 4r3 4b0u7 70 r473 7h3 f1|3 <i>{filename}</i>. P|3453 53|3(7 4 r471N6 b3|0W. 1 15 7h3 W0r57, 10 15 7h3 b357.";
+$str[rconf] = "Y0u h4V3 61V3N <i>{filename}</i> 4 r471N6 0f {rate}. 7h15 M4k35 7h3 f1|3'5 N3W r471N6 {nrating}/10.";
+$str[stats] = "5h0W 57475";
+$str[statstext] .= "7h3e3 4R3  $num[files] f1|35 1N  $num[cats] (47360r135<br>";
+$str[statstext] .= "7h3 0|d357 f1|3 15 <a href=\"pafiledb.php?action=file&id= $oldest[file_id]\"> $oldest[file_name]</a><br>";
+$str[statstext] .= "7h3 N3W357 f1|3 15 <a href=\"pafiledb.php?action=file&id= $newest[file_id]\"> $newest[file_name]</a><br>";
+$str[statstext] .= "7h3 1345t p0pu|4R f1|3 b453d 0n r471n65 15 <a href=\"pafiledb.php?action=file&id= $lpopular[file_id]\"> $lpopular[file_name]</a> w17h 4 r471n6 0f $least/10<br>";
+$str[statstext] .= "7h3 m057 p0pu|4R f1|3 b453d 0n r471n65 15 <a href=\"pafiledb.php?action=file&id= $popular[file_id]\"> $popular[file_name]</a> w17h 4 r471n6 0f  $most/10<br>";
+$str[statstext] .= "7h3 l3457 p0pu|4R f1|3 b453d 0n d0wn|04d5 15 <a href=\"pafiledb.php?action=file&id= $leastdl[file_id]\"> $leastdl[file_name]</a> w17h  $leastdl[file_dls] d0wn|04d5<br>";
+$str[statstext] .= "7h3 m057 p0pu|4R f1|3 b453d 0n d0wn|04d5 15 <a href=\"pafiledb.php?action=file&id= $mostdl[file_id]\"> $mostdl[file_name]</a> w17h  $mostdl[file_dls] d0wn|04d5<br>";
+$str[statstext] .= "7h3r3 h45 b33n $totaldls[0] 7074| d0wn|04d5<br>";
+$str[statstext] .= "7h3 4v3r463 f1|e r471n6 15  $avg/10<br>";
+$str[statstext] .= "7h3 4v3r463 4m0un7 0f d0wn|04d5 34(h f1|3 h45 15  $avgdls<br>";
+$str[search] = "534r(h";
+$str[results] = "R35u|75 f0r";
+$str[nomatches] = "50rry, N0 M47(h35 W3r3 f0uNd f0r";
+$str[matches] = "M47(h35 W3r3 f0uNd f0r";
+$str[sfor] = "534r(h F0r";
+$str[viewall] = "V13W 4|| F1|35";
+$str[vainfo] = "V13W 4|| 0f 7h3 f1|35 1N 7h3 d474b453";
+$str[next] = "N3x7";
+$str[prev] = "Pr3V10u5";
+$str[pagenums] = "P4635";
+//j00 h4v3 b33n h4x0r3d
+$str[acinfo] = "W3|(0M3 70 y0ur p4F1|3DB 3 4dM1N (3N73r. Y0u (4N u53 7h3 4dM1N (3N73r 70 M4N463 y0ur f1|3 d474b453 4Nd (h4N63 7h3 p4F1|3DB 53771N65. Y0u (4N 53|3(7 4 |1Nk 4b0V3 70 M4N463 y0ur d474b453.";
+$str[cmanage] = "(47360ry M4N463M3N7";
+$str[fmanage] = "F1|3 M4N463M3N7";
+$str[cumanage] = "(u570M F13|d5 M4N463M3N7";
+$str[lmanage] = "|1(3N53 M4N463M3N7";
+$str[amanage] = "4dM1N 4((0uN75 M4N463M3N7";
+$str[csettings] = "(h4N63 53771N65";
+$str[asettings] = "Y0ur 4((0uN7 53771N65";
+$str[utils] = "U71|17135";
+$str[aminfo] = "Y0u (4N u53 7h15 53(710N 0f 7h3 4dM1N (3N73r 70 4dd, 3d17, 0r d3|373 4dd1710N4| 4dM1N157r470r 4((0uN75.";
+$str[aadmin] = "4dd 4dM1N";
+$str[eadmin] = "3d17 4dM1N";
+$str[dadmin] = "D3|373 4dM1N";
+$str[un] = "U53rN4M3";
+$str[uninfo] = "3N73r 7h3 u53rN4M3 f0r 7h3 N3W 4((0uN7 h3r3";
+$str[aemail] = "4dM1N 3-M41| 4ddr355";
+$str[aemailinfo] = "3N73r 7h3 4dM1N'5 3-M41| 4ddr355 h3r3";
+$str[apass] = "P455W0rd";
+$str[apassinfo] = "3N73r 7h3 p455W0rd f0r 7h3 N3W 4dM1N 4((0uN7 h3r3. 7h15 (4N b3 (h4N63d 47 4Ny 71M3";
+$str[aeditperm] = "3d17 4dM1N5 P3rM15510N5";
+$str[aeditperminfo] = "53|3(7 1f y0u W4N7 7h3 N3W 4dM1N 70 b3 4b|3 70 4dd, 3d17, 4Nd d3|373 07h3r 4dM1N5.";
+$str[yes] = "Y35";
+$str[no] = "N0";
+$str[aadderror] = "7h3r3 15 4|r34dy 4N 4dM1N W17h 7h3 u53rN4M3 ";
+$str[adminadded] = "7h3 4dM1N <i>$form[username]</i> h45 b33N 4dd3d!";
+$str[changepass] = "(h4N63 P455W0rd";
+$str[newpass] = "N3W P455W0rd";
+$str[editerror] = "Y0u d1d N07 53|3(7 4N 4dM1N 70 3d17!";
+$str[infochanged] = "7h3 4dM1N'5 1Nf0 h45 b33N (h4N63d!";
+$str[passchanged] = "7h3 4dM1N'5 p455W0rd h45 b33N (h4N63d!";
+$str[delerror] = "Y0u d1dN'7 53|3(7 4Ny 4dM1N5 70 d3|373!";
+$str[deleted] = "7h3 4dM1N5 y0u 53|3(73d h4V3 b33N d3|373d.";
+$str[cmaninfo] = "Y0u (4N u53 7h3 (47360ry M4N463M3N7 53(710N 0f y0ur 4dM1N (3N73r 70 4dd, 3d17, d3|373 4Nd r30rd3r (47360r135. 1N 0rd3r 70 4dd f1|35 70 y0ur d474b453, y0u Mu57 h4V3 47 |3457 0N3 (47360ry (r3473d. Y0u (4N 53|3(7 4 |1Nk b3|0W 70 M4N463 y0ur (47360r135.";
+$str[acat] = "4dd (47360ry";
+$str[ecat] = "3d17 (47360ry";
+$str[dcat] = "D3|373 (47360ry";
+$str[rcat] = "R30rd3r (47360r135";
+$str[catadded] = "Y0ur N3W (47360ry, <i>$form[name]</i> h45 b33N 4dd3d!";
+$str[catname] = "(47360ry N4M3";
+$str[catnameinfo] = "7h15 W1|| b3(0M3 7h3 N4M3 0f 7h3 (47360ry.";
+$str[catdesc] = "(47360ry D35(r1p710N";
+$str[catdescinfo] = "7h15 15 4 d35(r1p710N 0f 7h3 f1|35 1N 7h3 (47360ry";
+$str[catparent] = "P4r3N7 (47360ry";
+$str[catparentinfo] = "1f y0u W4N7 7h15 (47360ry 70 b3 4 5ub-(47360ry, 53|3(7 7h3 (47360ry y0u W4N7 17 70 b3 4 5ub-(47360ry 0f.";
+$str[none] = "N0N3";
+$str[catedited] = "Y0ur (47360ry, <i>$form[name]</i> h45 b33N 3d173d!";
+$str[delfiles] = "D3|373 f1|35 1N (47360ry?";
+$str[catsdeleted] = "7h3 (47360r135 y0u 53|3(73d h4V3 b33N d3|373d.";
+$str[cdelerror] = "Y0u d1dN'7 53|3(7 4Ny (47360r135 70 d3|373!";
+$str[rcatinfo] = "Y0u (4N r30rd3r (47360r135 70 (h4N63 7h3 p051710N 7h3y 4r3 d15p|4y3d 1N 0N 7h3 M41N p463.70 r30rd3r 7h3 (47360r135, (h4N63 7h3 NuMb3r5 70 7h3 0rd3r y0u W4N7 7h3M 5h0WN 1N. 1 W1|| b3 5h0W3d f1r57, 2 W1|| b3 5h0WN 53(0Nd, 3(7. 7h15 d035 N07 4ff3(7 5ub-(47360r135.";
+$str[rcatdone] = "7h3 (47360r135 W3r3 r3-0rd3r3d!";
+$str[custominfo] = "Y0u (4N u53 7h3 (u570M f13|d5 M4N463M3N7 53(710N 0f 7h3 p4F1|3DB 4dM1N (3N73r 70 4dd, 3d17, 4Nd d3|373 (u570M f13|d5. Y0u (4N u53 (u570M f13|d5 70 4dd M0r3 1Nf0rM4710N 4b0u7 4 f1|3. F0r 3x4Mp|3, 1f y0u W4N7 4N 1Nf0rM4710N f13|d 70 pu7 7h3 f1|3'5 51z3 1N, y0u (4N (r3473 7h3 (u570M f13|d 4Nd 7h3N y0u (4N 4dd 7h3 f1|3 51z3 0N 7h3 4dd/3d17 f1|3 p463.";
+$str[afield] = "4dd F13|d";
+$str[efield] = "3d17 F13|d";
+$str[dfield] = "D3|373 F13|d";
+$str[fieldname] = "F13|d N4M3";
+$str[fieldnameinfo] = "7h15 15 7h3 N4M3 0f 7h3 f13|d, f0r 3x4Mp|3 'F1|3 51z3'";
+$str[fielddesc] = "F13|d D35(r1p710N";
+$str[fielddescinfo] = "7h15 15 4 d35(r1p710N 0f 7h3 f13|d, f0r 3x4Mp|3 'F1|3 51z3 1N M364by735'";
+$str[fieldadded] = "Y0ur N3W (u570M f13|d, <i>$form[name]</i> h45 b33N 4dd3d!";
+$str[fieldedited] = "Y0ur (u570M f13|d, <i>$form[name]</i> h45 b33N 3d173d!";
+$str[dfielderror] = "Y0u d1dN'7 53|3(7 4Ny f13|d5 70 d3|373!";
+$str[fieldsdel] = "Y0ur (u570M f13|d5 h4V3 b33N d3|373d!";
+$str[fmanageinfo] = "Y0u (4N u53 7h3 f1|3 M4N463M3N7 53(710N 0f 7h3 p4F1|3DB 4dM1N (3N73r 70 4dd, 3d17, 4Nd d3|373 f1|35.<br><b>71p:</b> 70 3451|y 3d17 0r d3|373 4 f1|3, M4k3 5ur3 y0u 4r3 |0663d 1N 45 4N 4dM1N 4Nd br0W53 70 4 f1|3 |1k3 4Ny0N3 N0rM4||y W0u|d 70 d0WN|04d 7h3 f1|3. 0N(3 0N 7h3 f1|3 1Nf0rM4710N p463, y0u W1|| h4V3 |1Nk5 70 7h3 f1|3'5 3d17 4Nd d3|373 p4635.";
+$str[afile] = "4dd F1|3";
+$str[efile] = "3d17 F1|3";
+$str[dfile] = "D3|373 F1|3";
+$str[upload] = "Up|04d F1|3";
+$str[uploadinfo] = "Up|04d 7h15 f1|3";
+$str[uploaderror] = "7h3 f1|3 $userfile_name 4|r34dy 3x1575! P|3453 r3N4M3 7h3 f1|3 4Nd 7ry 4641N.";
+$str[uploaddone] = "7h3 f1|3 $userfile_name h45 b33N up|04d3d! 7h3 UR| 70 7h3 f1|3 15";
+$str[uploaddone2] = "(|1(k h3r3 70 p|4(3 7h15 UR| 1N 7h3 D0WN|04d UR| f13|d.";
+$str[filename] = "F1|3 N4M3";
+$str[filenameinfo] = "7h15 15 7h3 N4M3 0f 7h3 f1|3 y0u 4r3 4dd1N6, 5u(h 45 'p4F1|3DB 3' 0r 'My P1(7ur3.'";
+$str[filesd] = "5h0r7 D35(r1p710N";
+$str[filesdinfo] = "7h15 15 4 5h0r7 d35(r1p710N 0f 7h3 f1|3. 7h15 W1|| 60 0N 7h3 p463 7h47 |1575 4|| 7h3 f1|35 1N 4 (47360ry, 50 7h15 d35(r1p710N 5h0u|d b3 5h0r7";
+$str[fileld] = "|0N6 D35(r1p710N";
+$str[fileldinfo] = "7h15 15 4 |0N63r d35(r1p710N 0f 7h3 f1|3. 7h15 W1|| 60 0N 7h3 f1|3'5 1Nf0rM4710N p463 50 7h15 d35(r1p710N (4N b3 |0N63r";
+$str[filecreator] = "(r3470r/4u7h0r";
+$str[filecreatorinfo] = "7h15 15 7h3 N4M3 0f Wh03V3r (r3473d 7h3 f1|3.";
+$str[fileversion] = "F1|3 V3r510N";
+$str[fileversioninfo] = "7h15 15 7h3 V3r510N 0f 7h3 f1|3, 5u(h 45 3.0 0r 1.3 B374";
+$str[filess] = "5(r33N5h07 UR|";
+$str[filessinfo] = "7h15 15 4 UR| 70 4 5(r33N5h07 0f 7h3 f1|3. F0r 3x4Mp|3, 1f y0u 4r3 4dd1N6 4 W1N4Mp 5k1N, 7h15 W0u|d b3 4 UR| 70 4 5(r33N5h07 0f W1N4Mp W17h 7h15 5k1N";
+$str[filedocs] = "D0(uM3N74710N/M4Nu4| UR|";
+$str[filedocsinfo] = "7h15 15 4 UR| 70 7h3 d0(uM3N74710N 0r 4 M4Nu4| f0r 7h3 f1|3";
+$str[fileurl] = "D0WN|04d UR|";
+$str[fileurlinfo] = "7h15 15 4 UR| 70 7h3 f1|3 7h47 W1|| b3 d0WN|04d3d. Y0u (4N 317h3r 7yp3 17 1N M4Nu4||y 0r <a href=\"javascript:NewWindow('pafiledb.php?action=admin&ad=file&file=upload','fileupload','600','450','custom','front');\">Up|04d 4 f1|3 70 7h3 53rV3r</a>";
+$str[filepi] = "P057 1(0N";
+$str[filepiinfo] = "Y0u (4N (h0053 4 p057 1(0N f0r 7h3 f1|3. 7h3 p057 1(0N W1|| b3 5h0WN N3x7 70 7h3 f1|3 1N 7h3 |157 0f f1|35.";
+$str[filecat] = "(47360ry";
+$str[filecatinfo] = "7h15 15 7h3 (47360ry 7h3 f1|3 b3|0N65 1N.";
+$str[filelicense] = "|1(3N53";
+$str[filelicenseinfo] = "7h15 15 7h3 |1(3N53 46r33M3N7 7h3 u53r Mu57 46r33 70 b3f0r3 d0WN|04d1N6 7h3 f1|3.";
+$str[filepin] = "P1N F1|3";
+$str[filepininfo] = "(h0053 1f y0u W4N7 7h3 f1|3 p1NN3d 0r N07. P1NN3d f1|35 W1|| 4|W4y5 b3 5h0WN 47 7h3 70p 0f 7h3 f1|3 |157.";
+$str[fileadded] = "Y0ur N3W f1|3, <i>$form[name]</i> h45 b33N 4dd3d!";
+$str[fileedited] = "Y0ur f1|3, <i>$form[name]</i> h45 b33N 3d173d!";
+$str[fderror] = "Y0u d1dN'7 53|3(7 4Ny f1|35 70 d3|373!";
+//14m3rZ
+$str[filesdeleted] = "7h3 f1|35 y0u 53|3(73d h4V3 b33N d3|373d!";
+$str[lmanageinfo] = "Y0u (4N u53 7h3 |1(3N53 M4N463M3N7 53(710N 0f 7h3 p4F1|3DB 4dM1N (3N73r 70 4dd, 3d17, 4Nd d3|373 |1(3N53 46r33M3N75. Y0u (4N 53|3(7 4 |1(3N53 f0r 4 f1|3 0N 7h3 f1|3 4dd 0r 3d17 p463. 1f 4 f1|3 h45 4 |1(3N53 46r33M3N7, 4 u53r W1|| h4V3 70 46r33 70 17 b3f0r3 d0WN|04d1N6 7h3 f1|3.";
+$str[alicense] = "4dd |1(3N53";
+$str[elicense] = "3d17 |1(3N53";
+$str[dlicense] = "D3|373 |1(3N53";
+$str[lname] = "|1(3N53 N4M3";
+$str[ltext] = "|1(3N53 73x7";
+$str[licenseadded] = "Y0ur N3W |1(3N53 46r33M3N7, <i>$form[name]</i> h45 b33N 4dd3d!";
+$str[licenseedited] = "Y0ur |1(3N53, <i>$form[name]</i> h45 b33N 3d173d!";
+$str[lderror] = "Y0u d1dN'7 53|3(7 4Ny |1(3N535 70 d3|373!";
+$str[ldeleted] = "7h3 |1(3N535 y0u 53|3(73d h4V3 b33N d3|373d.";
+$str[login] = "|06 1N";
+$str[username] = "U53rN4M3";
+$str[password] = "P455W0rd";
+$str[loggedin] = "Y0u h4V3 b33N |0663d 1N. <a href=\"pafiledb.php?action=admin&ad=main\">(|1(k h3r3</a> 70 4((355 y0ur 4dM1N (3N73r.";
+$str[loginerror] = "Y0u h4V3 3N73r3d 4 Wr0N6 u53rN4M3 0r p455W0rd!";
+$str[loggedout] = "Y0u h4V3 b33N |0663d 0u7.";
+$str[changeemail] = "(h4N63 3-M41| 4ddr355";
+$str[emailad] = "3-M41| 4ddr355";
+$str[confpass] = "(0Nf1rM P455W0rd";
+$str[nopermission] = "50rry, y0u d0 N07 h4V3 p3rM15510N5 70 u53 7h15 53(710N 0f 7h3 4dM1N (3N73r.";
+$str[emailchanged] = "Y0ur 3-M41| 4ddr355 h45 b33N (h4N63d!";
+$str[changepasserror] = "Y0ur p455W0rd 4Nd (0Nf1rM4710N p455W0rd d0 N07 M47(h. P|3453 60 b4(k 4Nd 7ry 4641N";
+$str[yourpasschanged] = "Y0ur p455W0rd h45 b33N (h4N63d!";
+$str[dbname] = "D474b453 N4M3";
+$str[dbnameinfo] = "7h15 15 7h3 N4M3 0f 7h3 d474b453, 5u(h 45 'W1Nd0W5 R36157ry h4(k5'";
+$str[dbdesc] = "D474b453 D35(r1p710N";
+$str[dbdescinfo] = "7h15 15 4 d35(r1p710N 0f 7h3 f1|35 1N 7h3 d474b453, 5u(h 45 'R36157ry h4(k5 7h47 M4k3 W1Nd0W5 u53fu|";
+$str[dburl] = "D474b453 UR|";
+$str[dburlinfo] = "7h15 15 7h3 UR| 70 7h3 d1r3(70ry Wh3r3 p4F1|3DB 15 1N574||3d";
+$str[topnum] = "70p NuMb3r";
+$str[topnuminfo] = "7h15 15 h0W M4Ny f1|35 W1|| b3 d15p|4y3d 0N 7h3 70p X D0WN|04d3d f1|35 |157";
+$str[hpurl] = "h0M3p463 UR|";
+$str[hpurlinfo] = "7h15 15 7h3 UR| 70 y0ur h0M3p463";
+$str[nfdays] = "N3W F1|3 D4y5";
+$str[nfdaysinfo] = "h0W M4Ny d4y5 N3W 4 f1|3 h45 70 b3 70 b3 |1573d W17h 4 'N3W F1|3' 1(0N. 1f 7h15 15 537 70 5, 7h3N 4|| f1|35 4dd3d W17h1N 7h3 p457 5 d4y5 W1|| h4V3 7h3 'N3W F1|3' 1(0N";
+$str[timeoffset] = "71M3 0ff537";
+$str[timeoffsetinfo] = "7h15 15 7h3 71M3 0ff537. F0r 3x4Mp|3, 1f 7h3 53rV3r 15 |0(473d 1N 7h3 34573rN 71M3 z0N3 bu7 y0u W4N7 7h3 71M35 70 b3 5h0WN 1N (3N7r4| 71M3, y0u W0u|d 537 7h15 70 -1";
+$str[tz] = "71M3 Z0N3";
+$str[tzinfo] = "7h15 15 7h3 71M3 z0N3 7h3 71M35 W1|| b3 5h0WN 1N";
+$str[header] = "h34d3r";
+$str[headerinfo] = "7h3 h34d3r f1|3 15 5h0WN b3f0r3 4Ny 0f 7h3 p4F1|3DB 0u7pu7 15 5h0WN";
+$str[footer] = "F0073r";
+$str[footerinfo] = "7h3 f0073r f1|3 15 5hW0N 4f73r 7h3 p4F1|3DB 0u7pu7 15 5h0WN.";
+$str[styleset] = "57y|3 537";
+$str[stylesetinfo] = "53|3(7 7h3 57y|3 537 y0u W15h 70 u53";
+$str[stats2] = "5h0w 57475";
+$str[statsinfo] = "(h0053 1f y0u w0u|d |1k3 70 5h0w 7h3 5(r1p7 3x3(u710n 57475 (7074| numb3r 0f My5Q| qu3r135 4nd h0w |0n6 17 700k f0r 7h3 5(r1p7 70 3x3(u73)";
+$str[settingschanged] = "Y0ur p4F1|3DB 53771N65 h4V3 b33N (h4N63d!";
+$str[lang] = "|4N6u463 F1|3";
+$str[langinfo] = "53|3(7 7h3 |4N6u463 f0r p4F1|3DB 70 u53.";
+
+//Start Beta 2.1 Language File
+$str[settings] = "53771N65";
+$str[pafdbsettings] = "p4F1|3DB 53771N65";
+
+//Start Beta 3 Language File
+$str[sortby] = "S0r7 By";
+$str[sort] = "S0r7";
+$str[name] = "N4m3";
+$str[bdb] = "B4(kup D474b453";
+$str[rdb] = "R3570r3 D474b453";
+$str[rdbinfo] = "j00 c4n r3570r3 4 p4F1l|3DB D474b453 b4(kup j00 m4d3 34r|13r. <b>N073:</b> 7h15 w1|| d3|373 4ny7h1ng 4|r34dy 1n y0ur D474b453 1n{|ud1ng 4dm1n 1nf0!<p>{h0053 4 f1|3 t00 r3570r3:";
+$str[rdbdone] = "y0ur d474b453 h45 b33n r357053d! 1f 4dm1n 1nf0 1n y0ur 0|d d474b453 4nd th3 d474b453 j00 ju57 r3570r3d 15 d1ff3r3nt, j00 m1gh7 h4v3 t00 l0g 1n 4g41n.";
+$str[home] = "H0m3p4g3";
+?>

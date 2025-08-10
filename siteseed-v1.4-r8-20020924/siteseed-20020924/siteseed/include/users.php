33fc51afa3b7d5118c077786c2b23597d6e086ed
me-suzy <me.suzana@gmail.com>
2025-08-11 00:59:01 +0300

    Add siteseed-v1.4-r8-20020924/siteseed-20020924/siteseed/include/users.php

---
+++ siteseed-v1.4-r8-20020924/siteseed-20020924/siteseed/include/users.php
@@ -0,0 +1,253 @@
+<?
+/**************************************
+
+Project: Siteseed (copyright MrNet 2001 - All right reserved)
+Filename: include/users.php
+Last modified: 20013103
+
+***************************************/ 
+
+
+// gets $param value from users table
+// from the currently logged user
+function get_user_data ($param="")
+{
+	if (!$param) return;
+	
+	global $session_id;
+	
+	// check if param exists
+	
+	if (! mysql_select_db (DB_NAME))
+	{
+		error (mysql_error()."\nwhile selecting database");
+	}
+
+	$query = mysql_query ("	SELECT field_name FROM user_fields
+	WHERE field_name='$param'");
+	
+	if (!$query) error (mysql_error());
+	
+	if (mysql_num_rows($query))
+	{
+		// get the value from users table
+		$query2 = mysql_query ("SELECT $param FROM users WHERE session_id='$session_id'");
+		
+		if (!$query2) error (mysql_error());
+		
+		// got value? return it
+		if (mysql_num_rows($query2))
+		{
+			list ($value) = mysql_fetch_row ($query2);
+			
+			return $value;
+		}
+	}
+	else
+	{
+		return;
+	}
+}
+
+
+// prints a form with the fields that are
+// required to login (login.php)
+// then we redirect the user to $url if login is ok
+// if $submit_image is set, it is used as a graphical
+// submit button, or,
+// if its not a image then it is used as the button string
+
+function print_login_form ($url="", $submit_image="")
+{
+	global $skin;
+	global $id;
+	
+	// get fields required to register
+	$query = mysql_query ("	SELECT field_name, code_login FROM user_fields
+	WHERE required_to_login=1
+	ORDER BY id");
+	
+	if (!$query) error (mysql_error());
+	
+	if (mysql_num_rows($query))
+	{
+		print "\n<form method=\"post\" action=\"login.php\">";
+		
+		while (list ($field_name, $code) = mysql_fetch_row($query))
+		{
+			$code = StripSlashes ($code);
+			
+			eval ("global $$field_name; ?>$code<?");
+		}
+		
+		
+		if ($submit_image)
+		{
+			if (	strstr ($submit_image, ".gif") ||
+			strstr ($submit_image, ".jpg") ||
+			strstr ($submit_image, ".png"))
+			{
+				print "\n<input type=image border=0 src=\"$submit_image\">";
+				
+			}
+			else
+			{
+				print "\n<input type=submit border=0 value=\"$submit_image\">";
+			}
+		}
+		else print "\n<input type=submit value=\"Entrar\">";
+		
+		if ($url) print "\n<input type=hidden name=url value=\"$url\">";
+		
+		print 	"\n<input type=hidden name=skin value=\"$skin\">".
+		"\n<input type=hidden name=id value=\"$id\">".
+		"\n</form>\n";
+	}
+}
+
+
+// prints a form with the fields that are required
+// to register (register_user.php)
+// then we redirect the user to $url if everything is ok
+// if $submit_image is set, it is used as a graphical
+// submit button, or,
+// if its not a image then it is used as the button string
+
+function print_register_form ($url="", $submit_image="")
+{
+	global $skin;
+	global $id;
+	
+	// get fields required to register
+	$query = mysql_query ("	SELECT field_name, code_register FROM user_fields
+	WHERE required_to_register=1
+	ORDER BY id");
+	
+	if (!$query) error (mysql_error());
+	
+	
+	if (mysql_num_rows($query))
+	{
+		print "\n<form method=\"post\" action=\"register_user.php\">";
+		
+		while (list ($field_name, $code) = mysql_fetch_row($query))
+		{
+			$code = StripSlashes ($code);
+			
+			eval ("global $$field_name; ?>$code<?");
+		}
+		
+		
+		if ($submit_image)
+		{
+			if (	strstr ($submit_image, ".gif") ||
+			strstr ($submit_image, ".jpg") ||
+			strstr ($submit_image, ".png"))
+			{
+				print "\n<input type=image border=0 src=\"$submit_image\">";
+			}
+			else
+			{
+				print "\n<input type=submit border=0 value=\"$submit_image\">";
+			}
+		}
+		else print "\n<input type=submit value=\"Registar\">";
+		
+		if ($url) print "\n<input type=hidden name=url value=\"$url\">";
+		
+		print 	"\n<input type=hidden name=skin value=\"$skin\">".
+		"\n<input type=hidden name=id value=\"$id\">".
+		"\n</form>\n";
+	}
+}
+
+
+
+// prints a form with the account fields
+// then we redirect the user to $url if everything is ok
+// if $submit_image is set, it is used as a graphical
+// submit button, or,
+// if its not a image then it is used as the button string
+
+function print_edit_account_form ($url="", $submit_image="")
+{
+	global $skin;
+	global $id;
+	
+	
+	// get fields that were required to register
+	$query = mysql_query ("	SELECT field_name, field_type, code_register FROM user_fields
+	WHERE required_to_register=1
+	ORDER BY id");
+	
+	if (!$query) error (mysql_error());
+	
+	
+	if (mysql_num_rows($query))
+	{
+		print "\n<form method=\"post\" action=\"edit_account.php\">";
+		
+		while (list ($field_name, $type, $code) = mysql_fetch_row($query))
+		{
+			$$field_name = get_user_data ($field_name);
+			
+			if ($type == "date")
+			{
+				$name_day = $field_name."_day";
+				$name_month = $field_name."_month";
+				$name_year = $field_name."_year";
+				
+				list ($$name_year, $$name_month, $$name_day) = explode ("-", $$field_name);
+			}
+			
+			$code = StripSlashes ($code);
+			
+			eval ("?>$code<?");
+		}
+		
+		if ($submit_image)
+		{
+			if (	strstr ($submit_image, ".gif") ||
+			strstr ($submit_image, ".jpg") ||
+			strstr ($submit_image, ".png"))
+			{
+				print "\n<input type=image border=0 src=\"$submit_image\">";
+			}
+			else
+			{
+				print "\n<input type=submit border=0 value=\"$submit_image\">";
+			}
+		}
+		else print "\n<input type=submit value=\"Alterar\">";
+		
+		if ($url) print "\n<input type=hidden name=url value=\"$url\">";
+		
+		print 	"\n<input type=hidden name=skin value=\"$skin\">".
+		"\n<input type=hidden name=id value=\"$id\">".
+		"\n</form>\n";
+	}
+}
+
+
+// logs out current user
+function log_out ()
+{
+	global $session_id;
+	
+	// kill the damn cookie
+	setcookie ("session_id");
+	
+	// clean the database entry
+	$query = mysql_query ("UPDATE users SET session_id=''");
+	
+	// clean the global variable
+	$session_id = "";
+}
+
+
+// prints "selected" if $value equals $field_value
+function selected ($field_value="", $value="")
+{
+	if ($field_value == $value) print " selected";
+}
+?>
